ce0a6d17a192f72c7dd4ea6582e83b57e0817a76
murrayds <dakota.s.murray@gmail.com>
2019-11-04 10:00:07 -0500

    Adjust umap script to work with parsed arguments

--- workflow/scripts/dimreduce_with_umap.py
+++ workflow/scripts/dimreduce_with_umap.py
@@ -7,30 +7,42 @@ author: Dakota Murray
 Conducts UMAP dimensioanlity reduction on the word2vec model.
 
 """
-import sys
 from gensim.models import Word2Vec
 import pandas as pd
 import umap
+import argparse
+
+
+# The number of components to reduce to
+N_COMPONENTS = 2
+
+parser = argparse.ArgumentParser()
 
 # System arguments
-# The path to the word2vec model
-WORD2VEC_MODEL_PATH = sys.argv[1]
+parser.add_argument("-mod", "--model", help="Path to word2vec model", type=str)
+parser.add_argument("-met", "--metric", help="Metric to use for UMAP", type = str)
+parser.add_argument("-n", "--neighbors", help="Number neighbors for UMAP", type=int)
+parser.add_argument("-d", "--mindistance", help="Minimum distance for UMAP", type=float)
+parser.add_argument("-o", "--output", help="Output data path", type=str)
 
-# The output data path
-OUTPUT_PATH = sys.argv[2]
+args = parser.parse_args()
 
 # Load the word2vec model
-model = Word2Vec.load(WORD2VEC_MODEL_PATH)
+model = Word2Vec.load(args.model)
 
 # Perform tsne dimensionality reduction
 # Build lists for the tokens and their labels
 tokens = [model[word] for word in model.wv.vocab]
 
 
-reducer = umap.UMAP()
+reducer = umap.UMAP(metric = args.metric,
+                    n_neighbors = args.neighbors,
+                    min_dist = args.mindistance,
+                    n_components = N_COMPONENTS
+                    )
 umap_embedding = reducer.fit_transform(tokens)
 umap_coords_df = pd.DataFrame(umap_embedding, columns = ['axis1', 'axis2'])
 umap_coords_df['token'] = [word for word in model.wv.vocab]
 
 # Save the output
-umap_coords_df.to_csv(OUTPUT_PATH)
+umap_coords_df.to_csv(args.output)
