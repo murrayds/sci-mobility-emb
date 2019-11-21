90d413c3c91102a5cfbe2841f9556739b803f29f
murrayds <dakota.s.murray@gmail.com>
2019-11-21 09:55:48 -0500

    Umap script now filters asccording to country.

--- workflow/scripts/dimreduce_with_umap.py
+++ workflow/scripts/dimreduce_with_umap.py
@@ -30,6 +30,10 @@ parser.add_argument("-n", "--neighbors", help = "Number neighbors for UMAP",
                     type = int, required = True)
 parser.add_argument("-d", "--mindistance", help = "Minimum distance for UMAP",
                     type = float, required = True)
+parser.add_argument("-c", "--country", help = "Country (iso code) to visualize",
+                    type = str, required = True)
+parser.add_argument("-l", "--lookup", help = "The lookup file containing organization information",
+                    type = str, required = True)
 parser.add_argument("-o", "--output", help = "Output data path",
                     type = str, required = True)
 
@@ -38,8 +42,17 @@ args = parser.parse_args()
 # Load the word2vec model
 model = Word2Vec.load(args.model)
 
-# Build lists for the tokens and their labels
-tokens = [model[word] for word in model.wv.vocab]
+# Build lists for the vectors and their labels
+vectors = [model[word] for word in model.wv.vocab]
+tokens = [word for word in model.wv.vocab]
+
+# If a country is specified, then filter accordingly.
+if args.country != "all":
+    lookup = pd.read_csv(args.lookup, sep = "\t")
+    orgs_in_country = list((lookup.cwts_org_no[lookup.country_iso_alpha == args.country]))
+    orgs_in_country = [str(org) for org in orgs_in_country] # convert to string
+    tokens = [word for word in tokens if word in orgs_in_country]
+    vectors = [model[word] for word in tokens]
 
 # Setup the UMAP reducer
 reducer = umap.UMAP(metric = args.metric,
@@ -47,13 +60,13 @@ reducer = umap.UMAP(metric = args.metric,
                     min_dist = args.mindistance,
                     n_components = N_COMPONENTS
                     )
-umap_embedding = reducer.fit_transform(tokens)
+umap_embedding = reducer.fit_transform(vectors)
 
 # Convert to pandas dataframe and assign axis names
 umap_coords_df = pd.DataFrame(umap_embedding, columns = [AXIS1_COLUMN_NAME, AXIS2_COLUMN_NAME])
 
 # Add the token (word) as a column, labelling each coordinate
-umap_coords_df[TOKEN_COLUMN_NAME] = [word for word in model.wv.vocab]
+umap_coords_df[TOKEN_COLUMN_NAME] = tokens
 
 # Save the output
 umap_coords_df.to_csv(args.output)
