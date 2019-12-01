7e6b94e4c84392fdff38dc5f061c8b68e2afe3a9
murrayds <dakota.s.murray@gmail.com>
2019-12-01 10:33:36 -0500

    Plot the reverse ECDF for the number of affiliations for mobile researchers

---
+++ workflow/scripts/PlotMobilityECDF.R
@@ -0,0 +1,74 @@
+#
+# PlotPubsOverTime.R
+#
+# author: Dakota Murray
+#
+# Plot the reverse ECDF of the number of affiliations for mobile researchers
+#
+
+# Plot dimensions
+FIG_WIDTH = 6
+FIG_HEIGHT = 5
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+suppressPackageStartupMessages(require(optparse))
+
+# Command line arguments
+option_list = list(
+  make_option(c("-i", "--input"), action="store", default=NA, type='character',
+              help="Path to file containing researcher metadata"),
+  make_option(c("-o", "--output"), action="store", default=NA, type='character',
+              help="Path to save output image")
+) # end option_list
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+researchers.meta <- read_delim(opt$input, delim = "\t", col_types = readr::cols()) %>%
+  select(cluster_id, num_org, num_city, num_region, num_country) %>%
+  filter(num_org > 1) # Filter to org-mobile researchers
+
+# Convert data to long format and add define factor levels
+researchers.meta.long <- researchers.meta %>%
+  tidyr::gather(key, value, num_org, num_city, num_region, num_country) %>%
+  mutate(
+    key = factor(key,
+                   levels = c("num_org", "num_city", "num_region", "num_country"),
+                   labels = c("Organization", "City", "Region", "Country"))
+  )
+
+
+# Separate df containing values for labels on the plot
+labels <- researchers.meta.long %>%
+  group_by(key) %>%
+  summarize(
+    ypos = (sum(value > 1) / n()),
+    cumulative = (sum(value > 1) / n())
+  )
+
+
+# Draw the plot
+plot <- researchers.meta.long %>%
+  ggplot(aes(x = value, group = key)) +
+  # Create a reverse ECDF Line
+  geom_step(aes(y = 1 - ..y..), stat='ecdf') +
+  # Add labels at x = 2 stating the proportion of mobile researchers
+  geom_label(data = labels,
+             aes(x = 2, y = ypos, label = format(round(cumulative, 2), nsmall = 2))
+  ) +
+  # Split into sub-plots for org, city, region, and country
+  facet_wrap(~key, nrow = 2, ncol = 2) +
+  scale_x_continuous(breaks = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), limits = c(0, 10)) +
+  scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1.0), limits = c(-0.1, 1.1)) +
+  theme_minimal() +
+  theme(
+    axis.title = element_text(size = 12, face = "bold"),
+    strip.text = element_text(size = 12, face = "bold"),
+  ) +
+  xlab("#Affiliations per researcher") +
+  ylab("Fraction of data")
+
+
+# Save the plot
+ggsave(opt$output, plot, width = FIG_WIDTH, height = FIG_HEIGHT)
