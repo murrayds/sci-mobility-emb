d51a1a97fe63b5e055b2bb2211539c7f869456cf
murrayds <dakota.s.murray@gmail.com>
2019-12-13 13:44:55 -0500

    Create network repersentation of flows

---
+++ workflow/scripts/ConvertFlowsToNetworkEdgelist.R
@@ -0,0 +1,32 @@
+#
+# ConvertFlowsToNetworkEdgelist.R
+#
+# author: Dakota Murray
+#
+# Convert the organization flows into an edgelist representation of a network
+# that can loaded into common graph software such as Gephi
+#
+
+library(dplyr)
+# Parse command line argument
+suppressPackageStartupMessages(require(optparse))
+
+# Command line arguments
+option_list = list(
+  make_option(c("-i", "--input"), action="store", default=NA, type='character',
+              help="Path to file containing organization flows"),
+  make_option(c("-o", "--output"), action="store", default=NA, type='character',
+              help="Path to save distances")
+) # end option_list
+opt = parse_args(OptionParser(option_list=option_list))
+
+# Get org lookup information
+flows <- readr::read_csv(opt$input, col_types = readr::cols()) %>%
+  # Convert names to Source, Target, and weight, which is what igraph expects
+  rename(Source = org1, Target = org2, weight = count) %>%
+  filter(weight > 0) %>% # remove 0 weights, they simply take up memory
+  filter(Source != Target) %>% # Remove self flows, they are equally not informative here
+  mutate(weight = 1 / weight) # we are calculating shortest past distances, so invert the weight
+
+# write the output
+readr::write_csv(flows, path = opt$output)
